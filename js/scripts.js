3a7324f1c9d1d1ff4a0523cf6148921ddb06d0a1
Mikerophonic <micgcon@gmail.com>
2023-06-13 15:14:55 -0700

    change event handler properties to event listeners, add fake ad

--- js/scripts.js
+++ js/scripts.js
@@ -1,13 +1,17 @@
-// UI Logic
+// User Interface Logic
+window.addEventListener("load", function() {
+  let form = document.querySelector("form");
+  let resetBtn = document.getElementById("reset");
+  let story = document.getElementById("story");
 
-function getAndSetMadLibValues() {
+  form.addEventListener("submit", function(event) {
     const person1Input = document.getElementById("person1Input").value;
     const person2Input = document.getElementById("person2Input").value;
     const animalInput= document.getElementById("animalInput").value;
     const exclamationInput = document.getElementById("exclamationInput").value;
     const verbInput = document.getElementById("verbInput").value;
     const nounInput = document.getElementById("nounInput").value;
-  
+
     document.querySelector("span#person1a").innerText = person1Input;
     document.querySelector("span#person1b").innerText = person1Input;
     document.querySelector("span#person1c").innerText = person1Input;
@@ -17,22 +21,26 @@ function getAndSetMadLibValues() {
     document.querySelector("span#verb").innerText = verbInput;
     document.querySelector("span#noun").innerText = nounInput;
     document.querySelector("span#exclamation").innerText = exclamationInput;
-  }
-  
-  // this function creates the onsubmit event handler
-  // it needs to be called in the window.onload event handler
-  function setFormSubmissionEventHandler() {
-    let form = document.querySelector("form");
-    form.onsubmit = function(event) {
-      event.preventDefault();
-      getAndSetMadLibValues();
-      document.querySelector("div#story").removeAttribute("class");
-    }
-  }
-  
-  window.onload = function() {
-    // inside of the window.onload handler function we only include
-    // the code that we want to run when
-    // the webpage has finished loading all resources
-    setFormSubmissionEventHandler();
-  };
+
+    story.removeAttribute("class");
+    event.preventDefault();
+  });
+
+  form.addEventListener("submit", function() {
+    resetBtn.removeAttribute("class");
+  }); 
+
+  form.addEventListener("submit", function() {
+    window.alert("Do you need a new computer? Visit www.superextracomputersales.com to find the best deals!");
+  }); 
+
+  resetBtn.addEventListener("click", function() {
+    story.setAttribute("class", "hidden");
+    document.getElementById("person1Input").value = null;
+    document.getElementById("person2Input").value = null;
+    document.getElementById("animalInput").value = null;
+    document.getElementById("exclamationInput").value = null;
+    document.getElementById("verbInput").value = null;
+    document.getElementById("nounInput").value = null;
+  });
+});
